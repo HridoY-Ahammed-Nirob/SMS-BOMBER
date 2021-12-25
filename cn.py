f7039c192fe9f3726982c4b6f0670d936d57d7ea
HridoY Ahammed Nirob <96482987+HridoY-Ahammed-Nirob@users.noreply.github.com>
2021-12-25 17:37:32 +0600

    Bomber
    Use for ligal

---
+++ cn.py
@@ -0,0 +1,22 @@
+import requests
+
+from requests.structures import CaseInsensitiveDict
+
+import os
+
+import sys
+
+import time
+
+
+os.system("clear")
+
+red="\033[0;31m"          # Red
+
+yellow="\033[0;33m"       # Yellow
+
+
+
+#cn.py
+
+print(red+"##################\n =============================\n Creator : HridoY Ahammed Nirob (Hacker)\n =============================\n Facebook : www.facebook.com/CYBER.HridoY.Ahammed.Nirob\n =============================\n Youtube : HridoY Ahammed Nirob\n =============================\n GITHUB : https://github.com/HridoY-Ahammed-Nirob\n =============================\n FB Page: www.facebook.com/ HridoY.Ahammed.Nirob.Official\n==========================================================")
